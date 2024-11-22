c7e056fbae824d595c7e5d4a68bf873214428bf0
ajmal-g4t <ajmalg4t@grow4tech.com>
2024-11-22 12:25:24 +0530

    preline changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import 'preline/dist/preline.js';
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
