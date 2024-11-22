f65c82ba539de9fac79e089dc75268c9113a7159
ajmal-g4t <ajmalg4t@grow4tech.com>
2024-11-22 12:15:28 +0530

    config

--- config.jsx
+++ config.jsx
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'https://cryptocheck-proto-1.onrender.com';
-export const BASE_URL = 'http://localhost:9098/api/v1';
-// export const BASE_URL = 'https://09e7-176-227-202-11.ngrok-free.app';
-export const FE_URL = 'http://localhost:5173';
+export const BASE_URL = 'https://formifybackend.onrender.com';
+// export const BASE_URL = 'http://localhost:9098/api/v1';
+export const FE_URL = 'https://formify-f44o.onrender.com';
+// export const FE_URL = 'http://localhost:5173';
