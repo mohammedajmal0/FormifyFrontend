b2b839cb3afab9688814ea1059578d5d91d93115
ajmal-g4t <ajmalg4t@grow4tech.com>
2024-11-22 12:40:05 +0530

    config

--- config.jsx
+++ config.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://formifybackend.onrender.com';
+export const BASE_URL = 'https://formifybackend.onrender.com/api/v1';
 // export const BASE_URL = 'http://localhost:9098/api/v1';
 export const FE_URL = 'https://formify-f44o.onrender.com';
 // export const FE_URL = 'http://localhost:5173';
